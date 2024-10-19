c3f266b909952024d800562f09288021b2360ac7
wangbo <wbo112@126.com>
2024-10-19 18:21:52 +0800

    server侧使用select模型

--- ServerDemo/ServerDemo.cpp
+++ ServerDemo/ServerDemo.cpp
@@ -62,6 +62,60 @@ struct LoginOutResult :public DataHeader {
 	int result;
 };
 
+#include <vector>
+std::vector<SOCKET> socket_set;
+
+
+int processor(SOCKET cSock) {
+	char szRecv[1024] = {};
+	int nLen = recv(cSock, (char*)szRecv, sizeof(DataHeader), 0);
+
+	DataHeader* header = (DataHeader*)szRecv;
+	if (nLen <= 0) {
+		std::cout << "client exit" << std::endl;
+		return -1;
+	}
+	if (nLen >= sizeof(DataHeader)) {
+
+	}
+
+	switch (header->cmd) {
+	case CMD_LOGIN:
+	{
+
+		recv(cSock, szRecv + sizeof(DataHeader), header->dataLength - sizeof(DataHeader), 0);
+		Login* login = (Login*)szRecv;
+		std::cout << "recv data : " << login->cmd << " length = " << login->dataLength << " username = " << login->userName << " password = " << login->passWord << std::endl;
+		LoginResult loginResult;
+
+		//send(cSock, (char*)&header, sizeof(DataHeader), 0);
+		send(cSock, (char*)&loginResult, sizeof(LoginResult), 0);
+	}
+	break;
+	case CMD_LOGINOUT:
+	{
+
+		recv(cSock, szRecv + sizeof(DataHeader), header->dataLength - sizeof(DataHeader), 0);
+		LoginOut* loginOut = (LoginOut*)szRecv;
+		std::cout << "recv data : " << loginOut->cmd << " length = " << loginOut->dataLength << " username = " << loginOut->userName << std::endl;
+		LoginOutResult loginOutResult = {};
+		loginOutResult.result = 1;
+
+		send(cSock, (char*)&loginOutResult, sizeof(LoginOutResult), 0);
+	}
+	break;
+	default:
+	{
+		DataHeader header;
+		header.cmd = CMD_ERR;
+		header.dataLength = 0;
+		send(cSock, (char*)&header, sizeof(DataHeader), 0);
+
+		std::cout << "switch default" << std::endl;
+	}
+	return 0;
+	}
+}
 int main() {
 	WORD ver = MAKEWORD(2, 2);
 	WSADATA dat;
@@ -92,67 +146,65 @@ int main() {
 		return 1;
 	}
 	std::cout << "listen succ" << std::endl;
-	SOCKET cSock = INVALID_SOCKET;
-	cSock = accept(sock, (sockaddr*)&clientAddr, &nAddrLen);
-	if (INVALID_SOCKET == cSock) {
-		std::cout << "accept fail" << std::endl;
-
-	}
-	else {
-		std::cout << "accept succ" << std::endl;
-	}
-	std::cout << "new client ip = " << inet_ntoa(clientAddr.sin_addr) << std::endl;;
 	while (true) {
-		char szRecv[1024] = {}; 
-		int nLen = recv(cSock, (char*)szRecv, sizeof(DataHeader), 0);
-		DataHeader*  header=(DataHeader*)szRecv ;
-		if (nLen <= 0) {
-			std::cout << "client exit" << std::endl;
+		fd_set readFd;
+		fd_set writeFd;
+		fd_set expFd;
+		FD_ZERO(&readFd);
+		FD_ZERO(&writeFd);
+		FD_ZERO(&expFd);
+
+		FD_SET(sock, &readFd);
+		FD_SET(sock, &writeFd);
+		FD_SET(sock, &expFd);
+		for (size_t n = 0; n < socket_set.size(); n++) {
+			FD_SET(socket_set[n], &readFd);
+		}
+		int max_fd = sock; // 初始化最大文件描述符为监听socket
+		// 遍历socket_set，更新最大文件描述符
+		for (size_t n = 0; n < socket_set.size(); n++) {
+			if (socket_set[n] > max_fd) {
+				max_fd = socket_set[n];
+			}
+		}
+		int ret = select(max_fd + 1, &readFd, &writeFd, &expFd, NULL);
+		if (ret < 0) {
+			std::cout << "select fail " << std::endl;
 			break;
 		}
-		if (nLen >= sizeof(DataHeader)) {
 
+		if (FD_ISSET(sock, &readFd)) {
+			FD_CLR(sock, &readFd);
+			sockaddr_in clientAddr = {};
+			int nAddrLen = sizeof(sockaddr_in);
+			SOCKET sock_client = INVALID_SOCKET;
+			sock_client = accept(sock, (sockaddr*)&clientAddr, &nAddrLen);
+
+
+			std::cout << "new client ip = " << inet_ntoa(clientAddr.sin_addr) << std::endl;;
+			if (INVALID_SOCKET == sock_client) {
+				std::cout << "accept fail" << std::endl;
+			}
+			std::cout << "new socket add" << std::endl;
+			socket_set.push_back(sock_client);
 		}
-		switch (header->cmd) {
-		case CMD_LOGIN:
-		{
+		for (size_t n = 0; n < readFd.fd_count; n++) {
 
-			recv(cSock, szRecv + sizeof(DataHeader), header->dataLength - sizeof(DataHeader), 0);
-			Login* login=(Login*)szRecv;
-			std::cout << "recv data : " << login->cmd << " length = " << login->dataLength << " username = " << login->userName << " password = " << login->passWord << std::endl;
-			LoginResult loginResult;
-
-			//send(cSock, (char*)&header, sizeof(DataHeader), 0);
-			send(cSock, (char*)&loginResult, sizeof(LoginResult), 0);
-		}
-		break;
-		case CMD_LOGINOUT:
-		{
-
-			recv(cSock, szRecv + sizeof(DataHeader), header->dataLength - sizeof(DataHeader), 0);
-			LoginOut* loginOut=(LoginOut*)szRecv;
-			std::cout << "recv data : " << loginOut->cmd << " length = " << loginOut->dataLength << " username = " << loginOut->userName << std::endl;
-			LoginOutResult loginOutResult = {};
-			loginOutResult.result = 1;
-
-			send(cSock, (char*)&loginOutResult, sizeof(LoginOutResult), 0);
-		}
-		break;
-		default:
-		{
-			DataHeader header;
-			header.cmd = CMD_ERR;
-			header.dataLength = 0;
-			send(cSock, (char*)&header, sizeof(DataHeader), 0);
-
-			std::cout << "switch default" << std::endl;
-		}
+			if (-1 == processor(readFd.fd_array[n])) {
+				auto iter = find(socket_set.begin(), socket_set.end(), readFd.fd_array[n]);
+				if (iter != socket_set.end()) {
+					socket_set.erase(iter);
+				}
+			}
 		}
 
 
 
 	}
-	closesocket(cSock);
+	for (size_t n = 0; n < socket_set.size(); n++) {
+		closesocket(socket_set[n]);
+
+	}
 	closesocket(sock);
 	WSACleanup();
 	return 0;
